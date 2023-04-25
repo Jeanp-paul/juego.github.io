6c9bf77a0348d58f267790a7d10a7bc762c28433
jeanp <jpoyola@misena.edu.co>
2023-04-25 09:18:30 -0500

    dia 2

--- base-de.preguntas.js
+++ base-de.preguntas.js
@@ -12,15 +12,40 @@ let baseDePreguntas = [
     ayuda:"No es reglamentaria",
     imagen:"https://i.ibb.co/N2DHWTr/7461d2c303ddf4e19528575c2b6d73e0.jpg",
     respuesta: "Informativa",
-    distractores: ["Preventiva","Informativa","transitoria",
+    distractores: ["Preventiva","Reglamentaria","transitoria",
 ]
 },
 {
-    pregunta:"¿que tipo de señal es ?",
-    ayuda:"No es reglamentaria",
-    imagen:"https://i.ibb.co/N2DHWTr/7461d2c303ddf4e19528575c2b6d73e0.jpg",
-    respuesta: "Informativa",
-    distractores: ["Preventiva","Informativa","transitoria",
+    pregunta:"¿Si quedamos sobre esta señal regtangular debemos ?",
+    ayuda:"No podemos retroceder",
+    imagen:"https://i.ibb.co/Rpf3cyH/20220330-102713627-56b1318dc09f0-r-1454507139905-6-193-695-540-jpeg-982300513.webp",
+    respuesta: "Avanzar",
+    distractores: ["Retroceder","Quedarnos quietos","Ningunas de las anteriores",
+]
+},
+{
+    pregunta:"¿Que objetos pertenecen al kit de carreteras?",
+    ayuda:"No son dos llantas",
+    imagen:"https://i.ibb.co/9nQnvRX/kit-de-carretera-pilot-racing-reglamentario-1.webp",
+    respuesta: "Linterna,Llanta de repuesto,Extintor",
+    distractores: ["Dos llantas,Dos crucetas","Caja de copas,Calibrador de llantas","Ningunas de las anteriores",
+]
+},
+{
+    pregunta:"¿Que tipo de licencia requieres para poder manejar este vehiculo?",
+    ayuda:"No es A2",
+    imagen:"https://i.ibb.co/4RkvxXH/como-sacar-licencia-c3-colombia.jpg",
+    respuesta: "C3",
+    distractores: ["C1","C2","A2",
+]
+},
+{
+    pregunta:"¿Que objetos pertenecen al kit de primeros auxilios?",
+    ayuda:"no son las Toallas ",
+    imagen:"https://i.ibb.co/mTJCZRY/D-NQ-NP-609817-MCO31631282377-072019-O.jpg",
+    respuesta: "Gasa esteril,tijeras",
+    distractores: ["Toalla higienica,condones","medicamentos","todas las anteriores",
 ]
 },
 ]
+
